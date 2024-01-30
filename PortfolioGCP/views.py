ebeba0611aebb1ab3edc1ca48e5eeb032372f494
DianaMSanchez <dianam.sanchez@gmail.com>
2024-01-30 18:57:33 +0100

    Primeros ficheros

---
+++ PortfolioGCP/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+import datetime
+
+
+def presentacion(request): #Presentación 
+    date_time = datetime.datetime.now()
+    documento = open("C:/Proyectos Django/PortfolioGCP/PortfolioGCP/template/presentacion.html")
+    date = Template(documento.read())
+    documento.close()
+    ctx = Context()
+    documento_Renderizado = date.render(ctx)
+    return HttpResponse(documento_Renderizado)
+
+def proyecto(request): #Proyecto 
+
+    return HttpResponse("Aquí van los proyectos")
+
+def estudios(request): #Estudios 
+
+    return HttpResponse("Estudios")
+
+def publicaciones(request): #Publicaciones 
+
+    return HttpResponse("Publicaciones")
+
+def redes_sociales(request): #Redes sociales 
+
+    return HttpResponse("Redes sociales")
+
+                        
+
+
