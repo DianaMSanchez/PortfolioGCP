c04fbb9421b8a7163b61940f56ba531bef5db280
marisa <mgarciagarcia53@uoc.edu>
2024-02-02 15:06:35 +0100

    form contacto

--- PortfolioGCP/urls.py
+++ PortfolioGCP/urls.py
@@ -15,15 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from portafolio import views 
+from contacto import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.presentacion, name='presentacion')
+    path('', views.presentacion, name='presentacion'),
+    path('contacto/', include('contacto.urls')),
     #path ('presentacion/', presentacion),
     #path ('proyecto/', proyecto),
     #path ('estudios/', estudios),
--- contacto/models.py
+++ contacto/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+opciones_consultas = [
+    [0, "consulta"],
+    [1, "sugerencia"],
+    [2, "enhorabuena"]
+]
+
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+    tipo_consulta = models.IntegerField(choices= opciones_consultas)
+    mensaje = models.TextField()
+    avisos = models.BooleanField()
+    date = models.DateField(datetime.date.today)
+
+    def __str__(self):
+        return self.nombre
+
